2784b3a825a7e6fe7fa192d31380ba7b97c2ab43
gostavolz <gustavoluiz13531@gmail.com>
2025-10-07 10:15:16 -0300

    Add files via upload

---
+++ Python_New_Salary/Python_New_Salary/Python_New_Salary.py
@@ -0,0 +1,5 @@
+old_Salary = float(input("Informe o valor do seu salário anterior: "))
+
+new_Salary = old_Salary + (old_Salary * 0.25)
+
+print(f"O valor de seu novo salario é de: R${new_Salary:.2f}")
